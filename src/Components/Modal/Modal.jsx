1ace67dc7e4d74e72ccfaa4378064771589c2fb6
Danylo Nehreba <butanasiland@gmail.com>
2021-12-21 21:48:06 +0200

    Fix modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,41 +1,41 @@
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import style from './Modal.module.scss';
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-	static propTypes = {
-		onClose: PropTypes.func.isRequired,
-		src: PropTypes.string.isRequired,
-		alt: PropTypes.string.isRequired,
-	};
-	componentDidMount() {
-		window.addEventListener('keydown', this.closeModal);
-	}
-	componentWillUnmount() {
-		window.removeEventListener('keydown', this.closeModal);
-	}
-	closeModal = e => {
+export function Modal({ onClose, src, alt }) {
+	useEffect(() => {
+		window.addEventListener('keydown', closeModal);
+		return () => {
+			window.removeEventListener('keydown', closeModal);
+		};
+	});
+
+	const closeModal = e => {
 		if (e.code === 'Escape') {
-			this.props.onClose();
-		}
-	};
-	handleClick = event => {
-		if (event.currentTarget === event.target) {
-			this.props.onClose();
+			onClose();
 		}
 	};
 
-	render() {
-		const { src, alt } = this.props;
-		return createPortal(
-			<div className={style.Overlay} onClick={this.handleClick}>
-				<div className={style.Modal}>
-					<img src={src} alt={alt} />
-				</div>
-			</div>,
-			modalRoot,
-		);
-	}
+	const handleClick = event => {
+		if (event.currentTarget === event.target) {
+			onClose();
+		}
+	};
+
+	return createPortal(
+		<div className={style.Overlay} onClick={handleClick}>
+			<div className={style.Modal}>
+				<img src={src} alt={alt} />
+			</div>
+		</div>,
+		modalRoot,
+	);
 }
+
+Modal.propTypes = {
+	onClose: PropTypes.func.isRequired,
+	src: PropTypes.string.isRequired,
+	alt: PropTypes.string.isRequired,
+};
